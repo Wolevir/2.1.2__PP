7f0e895919afec7184d14e294f403e36c6cfa733
Wolevir <100397898+Wolevir@users.noreply.github.com>
2022-08-25 13:43:03 +0400

    Add files via upload

---
+++ 2.1.2__PP/src/main/java/App.java
@@ -0,0 +1,19 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean1.getMessage());
+        System.out.println(bean2.getMessage());
+        System.out.println(bean3.getMassage());
+        System.out.println(bean4.getMassage());
+        System.out.println(bean1.equals(bean2));
+        System.out.println(bean3.equals(bean4));
+    }
+}
